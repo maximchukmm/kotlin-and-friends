4922d687c8e59c405b3a30593c559d2829ea7557
mishamm <maximchuk.mihail@gmail.com>
2020-04-14 23:14:43 +0300

    converted Hello class to data class module hibernate

--- hibernate/src/test/kotlin/edu/hibernate/helloworld/HelloTest.kt
+++ hibernate/src/test/kotlin/edu/hibernate/helloworld/HelloTest.kt
@@ -20,7 +20,7 @@ class HelloTest : HibernateBaseTest() {
 
         doInTransaction { session ->
             run {
-                session?.persist(Hello(greetings))
+                session?.persist(Hello(null, greetings))
             }
         }
 
@@ -30,45 +30,20 @@ class HelloTest : HibernateBaseTest() {
 
                 val foundEntities = selectAllQuery?.resultList
 
-                log.info("Found entities: {}", foundEntities.toString())
-
                 assertTrue { foundEntities?.size == 1 }
 
                 assertEquals(greetings, foundEntities?.get(0)?.greetings)
-
             }
         }
     }
 
     @Entity(name = "Hello")
     @Table(name = "hello")
-    private class Hello(greetings: String?) {
-        @Id
-        @GeneratedValue(strategy = GenerationType.AUTO)
-        var id: Long? = null
+    private class Hello(
+            @Id
+            @GeneratedValue(strategy = GenerationType.AUTO)
+            var id: Long?,
 
-        var greetings: String? = greetings
-
-        override fun equals(other: Any?): Boolean {
-            if (this === other) return true
-            if (javaClass != other?.javaClass) return false
-
-            other as Hello
-
-            if (id != other.id) return false
-            if (greetings != other.greetings) return false
-
-            return true
-        }
-
-        override fun hashCode(): Int {
-            var result = id?.hashCode() ?: 0
-            result = 31 * result + (greetings?.hashCode() ?: 0)
-            return result
-        }
-
-        override fun toString(): String {
-            return "Hello(id=$id, greetings=$greetings)"
-        }
-    }
+            var greetings: String?
+    )
 }
