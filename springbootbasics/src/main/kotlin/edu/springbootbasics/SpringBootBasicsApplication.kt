3f5a0df2870092f058e0dfd5dad09abf13f5b9f3
mishamm <maximchuk.mihail@gmail.com>
2020-04-14 20:09:08 +0300

    added basic WebConfig

--- springbootbasics/src/main/kotlin/edu/springbootbasics/SpringBootBasicsApplication.kt
+++ springbootbasics/src/main/kotlin/edu/springbootbasics/SpringBootBasicsApplication.kt
@@ -1,17 +1,11 @@
 package edu.springbootbasics
 
-import edu.springbootbasics.model.AuthorService
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.ApplicationContext
 
 @SpringBootApplication
 class SpringBootBasicsApplication
 
 fun main(args: Array<String>) {
-    val applicationContext: ApplicationContext = runApplication<SpringBootBasicsApplication>(*args)
-
-    val authorService: AuthorService = applicationContext.getBean(AuthorService::class.java)
-
-    authorService.printAuthorByName("Ivanov I.")
+    runApplication<SpringBootBasicsApplication>(*args)
 }
